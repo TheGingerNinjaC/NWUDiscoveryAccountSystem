4418ba2f8989e8f0fad2ea3190b095aa00a76b95
Chane <vanderbergchane@gmail.com>
2021-10-05 17:57:23 +0200

    Added more swagger settings

--- Web/src/main/java/za/ac/nw/discovery/web/config/WebConfig.java
+++ Web/src/main/java/za/ac/nw/discovery/web/config/WebConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"za.ac.nw.discovery.web.controller"})
+@ComponentScan(basePackages = {
+        "za.ac.nw.discovery.web.controller",
+        "za.ac.nw.discovery.web.exception"
+})
 public class WebConfig {
 }
--- Web/src/main/java/za/ac/nw/discovery/web/controller/DemoController.java
+++ Web/src/main/java/za/ac/nw/discovery/web/controller/DemoController.java
@@ -1,14 +1,23 @@
 package za.ac.nw.discovery.web.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import za.ac.nw.discovery.domain.service.GeneralResponse;
 
 @RestController
+@RequestMapping("demo")
 public class DemoController {
 
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
 }
